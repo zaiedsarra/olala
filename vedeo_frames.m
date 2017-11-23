7155fd3303eab630a9ec2774adc9634540a11379
zaiedsarra <33926948+zaiedsarra@users.noreply.github.com>
2017-11-23 13:09:44 +0100

    erfgzercf
    erfverfr

---
+++ vedeo_frames.m
@@ -0,0 +1,13 @@
+clc 
+clear all
+close all
+fileName = 'C:\Users\PARIS-GSM\Desktop\Videos\vedeo1796\Sessions\1796\S030-007.avi';
+obj = VideoReader(fileName);
+numFrames = obj.NumberOfFrames;
+ for kk =1 : numFrames
+     fig=figure;
+     frame = read(obj,kk);
+     imshow(frame);
+     saveas(fig,['C:\Users\PARIS-GSM\Desktop\Videos\vedeo1796\frames\',num2str(kk)],'jpeg');
+     close all 
+ end
